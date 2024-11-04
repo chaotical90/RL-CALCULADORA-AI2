7105b1a47c16960f47f67006d465e25e18e68adb
PAI DO FLÁVIO <49536075+chaotical90@users.noreply.github.com>
2024-11-04 12:36:36 +0000

    divisões e multiplicações

--- calculadora.py
+++ calculadora.py
@@ -5,3 +5,14 @@ def soma(x, y):
 # Função para subtração
 def subtracao(x, y):
     return x - y
+
+# Função para divisão
+def divisao(x, y):
+    if y != 0:
+        return x / y
+    else:
+        return "Erro: Divisão por zero não é permitida."
+# Função para multiplicação
+def multiplicacao(x, y):
+    return x * y
+    
